89e152e5302648fbff3ad2cbc593ad814f26d359
feitosa112 <112kuzmanovic@gmail.com>
2023-12-10 14:25:33 +0100

    logo

--- resources/views/searchCard.blade.php
+++ resources/views/searchCard.blade.php
@@ -1,4 +1,12 @@
 <div class="row">
+    <div class="col-12">
+        <h5 class="slikalogo1">Moj online shop</h5>
+    </div>
+</div>
+<div class="row">
+    <div class="col-3">
+        <h4 class="slikalogo">Moj online shop</h4>
+    </div>
     <div class="col-lg-6 col-md-6 col-sm-6 mx-auto">
         <form action="{{route('search')}}">
             <div class="input-group mb-3">
